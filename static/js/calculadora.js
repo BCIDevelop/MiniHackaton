0b78adf6217ec505d695b47ef0224842a73ee250
Luis Lopez <luis.lopez@utec.edu.pe>
2022-11-05 19:25:53 -0500

    footer

---
+++ static/js/calculadora.js
@@ -0,0 +1,16 @@
+btnComprobar=document.getElementById('btn-comprobar')
+input=document.getElementById('input')
+result = document.getElementById('display')
+
+function isLeapYear(value){
+   
+    if(value%100!==0 || value%400===0 || value/4) return true 
+    return false
+}
+
+btnComprobar.onsubmit=function(e){
+    e.preventDefualt()
+    
+    if(isLeapYear(input.value)) result.innerHTML='Si es año bisiesto' 
+    else result.innerHTML='No es año bisiesto' 
+}
---
+++ static/js/calculadora2.js
@@ -0,0 +1,4 @@
+function calculadora(operacion,x,y)
+{
+    
+}
